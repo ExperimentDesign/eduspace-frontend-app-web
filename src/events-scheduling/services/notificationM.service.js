9cef08d21effe1e18cf4cce312427960166c3ebc
Andres Torres <andresalbertotorresgarcia@gmail.com>
2025-09-18 06:37:27 -0500

    refactor: remove payroll management and related components from the project

--- src/events-scheduling/services/notificationM.service.js
+++
@@ -1,9 +0,0 @@
-import http from "../../shared/services/http-common.js";
-
-export class NotificationMService {
-    resourceEndpoint = "/notifications";
-
-    createNotification(notification) {
-        return http.post(this.resourceEndpoint, notification);
-    }
-}
--- src/personal-management/services/notificationT.service.js
+++
@@ -1,9 +0,0 @@
-import http from "../../shared/services/http-common.js";
-
-export class NotificationService {
-    resourceEndpoint = "/notifications";
-
-    createNotification(notification) {
-        return http.post(this.resourceEndpoint, notification);
-    }
-}
